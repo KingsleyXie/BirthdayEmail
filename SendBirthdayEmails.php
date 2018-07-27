1a059b9934fd0e04898ae78a4bf08c5e951560c9
Kingsley <kingsley.xym@gmail.com>
2018-07-28 01:52:04 +0800

    :sparkles: Prepare Framework Of TextSizeParser

--- SendBirthdayEmails.php
+++ SendBirthdayEmails.php
@@ -1,10 +1,12 @@
 <?php
 require_once 'FontLib\Autoloader.php';
 require_once 'Tools\FontChecker.php';
+require_once 'Tools\TextSizeParser.php';
 
 use Tools\FontChecker;
+use Tools\TextSizeParser;
 
-// header('Content-type: image/png');
+header('Content-type: image/png');
 
 $image = imagecreatefrompng('Assets\card.png');
 $color = imagecolorallocate($image, 144, 139, 134);
@@ -16,11 +18,13 @@ $text = "董建华";
 
 // Check if current font supports the text
 $checker = new FontChecker;
-$checker->batchTest([$font_path, $font_path_alternate]);
+if (!$checker->isStringValid($text, $font_path)) $font_path = $font_path_alternate;
 
-// if (!$checker->test($text, $font_path)) $font_path = $font_path_alternate;
-/*
-imagettftext($image, 80, 0, 652, 154, $color, $font_path, $text);
-imagepng($image);
-imagedestroy($image);
-*/
+$textSize = new TextSizeParser;
+$ts = $textSize->calc($text, true);
+
+imagettftext($image, $ts[0], 0, $ts[1], $ts[2], $color, $font_path, $text);
+imagepng($image); imagedestroy($image);
+
+// Test Code
+// $checker->batchTest([$font_path, $font_path_alternate]);

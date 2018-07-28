d3969acc0614d907b44c2d4a6902e9b17e29a8aa
Kingsley <kingsley.xym@gmail.com>
2018-07-28 11:58:41 +0800

    :recycle: Integrate The Image Generate Procedure As A Method

--- SendBirthdayEmails.php
+++ SendBirthdayEmails.php
@@ -6,48 +6,49 @@ require_once 'Tools\TextDataParser.php';
 use Tools\FontChecker;
 use Tools\TextDataParser;
 
-$debugging = false;
-if (!$debugging) header('Content-type: image/png');
+class SendBirthdayEmails
+{
+    public function generateImage($name, $dir) {
+        $image = imagecreatefrompng('Assets\card.png');
+        $color = imagecolorallocate($image, 144, 139, 134);
 
-$image = imagecreatefrompng('Assets\card.png');
-$color = imagecolorallocate($image, 144, 139, 134);
+        $asset_path = getcwd() . '\Assets';
+        $font_path = $asset_path . '\name.ttf';
+        $font_path_alternate = $asset_path . '\alt.ttf';
 
-$asset_path = getcwd() . '\Assets';
-$font_path = $asset_path . '\name.ttf';
-$font_path_alternate = $asset_path . '\alt.ttf';
+        $checker = new FontChecker;
+        $parser = new TextDataParser;
 
-$text = "拉斯特洛堃斯基·赵五六七八";
+        // Test Code
+        // $checker->batchTest([$font_path, $font_path_alternate]);
+        // $parser->test();
 
-$checker = new FontChecker;
-$parser = new TextDataParser;
+        $td = null;
+        if ($checker->isStringValid($name, $font_path)) {
+            // If current font supports the text
+            // Calculate Text Related Data(Size & Position)
+            $td = $parser->calc($name);
+        } else {
+            $font_path = $font_path_alternate;
+            $td = $parser->calcAltFont($name);
+        }
 
-// Test Code
-if ($debugging) {
-    $checker->batchTest([$font_path, $font_path_alternate]);
-    $parser->test();
+        imagettftext(
+            $image,
+            $td->size,
+            0,
+            $td->paddingLeft,
+            $td->paddingTop,
+            $color,
+            $font_path,
+            str_replace('·', "\n", $name)
+        );
+
+        imagepng($image, "$dir$name.png");
+        imagedestroy($image);
+    }
 }
 
-$td = null;
-if ($checker->isStringValid($text, $font_path)) {
-    // If current font supports the text
-    // Calculate Text Related Data(Size & Position)
-    $td = $parser->calc($text);
-} else {
-    $font_path = $font_path_alternate;
-    $td = $parser->calcAltFont($text);
-}
-
-imagettftext(
-    $image,
-    $td->size,
-    0,
-    $td->paddingLeft,
-    $td->paddingTop,
-    $color,
-    $font_path,
-    str_replace('·', "\n", $text)
-);
-
-if (!$debugging) imagepng($image);
-
-imagedestroy($image);
+$name = '董华';
+$obj = new SendBirthdayEmails;
+$obj->generateImage($name, 'GeneratedImages/');

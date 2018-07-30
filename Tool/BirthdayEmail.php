bf8939ba6584ff642a0001566b2bce08dd72d350
Kingsley <kingsley.xym@gmail.com>
2018-07-30 16:43:21 +0800

    :bug: Fix A Mailer Initialize Related Bug

--- Tool/BirthdayEmail.php
+++ Tool/BirthdayEmail.php
@@ -7,67 +7,79 @@ use PHPMailer\PHPMailer\Exception;
 
 class BirthdayEmail
 {
-    private $mail;
+    private $mailer;
 
-    public function __construct() {
+    private function initialize() {
         $conf = Config::$mail;
 
-        $this->mail = new PHPMailer(true);
-        $this->mail->isSMTP();
+        $this->mailer = new PHPMailer(true);
+        $this->mailer->isSMTP();
 
-        $this->mail->Host = $conf['host'];
-        $this->mail->SMTPAuth = true;
-        $this->mail->Username = $conf['username'];
-        $this->mail->Password = $conf['password'];
-        $this->mail->SMTPSecure = $conf['secure'];
-        $this->mail->Port = $conf['port'];
+        $this->mailer->Host = $conf['host'];
+        $this->mailer->SMTPAuth = true;
+        $this->mailer->Username = $conf['username'];
+        $this->mailer->Password = $conf['password'];
+        $this->mailer->SMTPSecure = $conf['secure'];
+        $this->mailer->Port = $conf['port'];
 
-        $this->mail->CharSet = 'UTF-8';
-        $this->mail->setFrom($conf['address'], $conf['name']);
+        $this->mailer->CharSet = 'UTF-8';
+        $this->mailer->setFrom($conf['address'], $conf['name']);
     }
 
     public function sendBirthdayCard($addr, $card, $line) {
+        $this->initialize();
+
         $conf = Config::$mail['card'];
         try {
-            $this->mail->addAddress($addr);
+            $this->mailer->addAddress($addr);
 
-            $this->mail->Subject = $conf['subject'];
-            $this->mail->addEmbeddedImage($card, 'card'); // Use `cid:card` as value of src in HTML code
-            $this->mail->addEmbeddedImage($line, 'line'); // Use `cid:line` as value of src in HTML code
+            $this->mailer->Subject = $conf['subject'];
+            $this->mailer->addEmbeddedImage($card, 'card'); // Use `cid:card` as value of src in HTML code
+            $this->mailer->addEmbeddedImage($line, 'line'); // Use `cid:line` as value of src in HTML code
 
-            $this->mail->Body = $conf['body'];
-            $this->mail->AltBody = $conf['altbody'];
-            $this->mail->isHTML(true);
+            $this->mailer->Body = $conf['body'];
+            $this->mailer->AltBody = $conf['altbody'];
+            $this->mailer->isHTML(true);
 
-            $this->mail->send();
+            $this->mailer->send();
         } catch (Exception $e) {
-            throw new \Exception($this->mail->ErrorInfo);
-        }
-    }
-
-    public function sendErrorMsg($msg) {
-        $conf = Config::$mail['error'];
-        foreach ($conf['admin'] as $addr) {
-            try {
-                $this->mail->addAddress($addr);
-                $this->mail->Subject = $conf['subject'];
-                $this->mail->Body = $msg;
-                $this->mail->send();
-            } catch (Exception $e) {
-                throw new \Exception($this->mail->ErrorInfo);
-            }
+            throw new \Exception($this->mailer->ErrorInfo);
         }
     }
 
     public function sendRemind($ministers, $subject, $msg) {
+        $this->initialize();
+
         foreach ($ministers as $minister) {
             try {
-                $this->mail->addAddress($minister['email']);
-                $this->mail->Subject = $subject;
-                $this->mail->Body = $msg;
-                $this->mail->send();
+                // Former settings should be cleared inside a loop
+                $this->mailer->clearAddresses();
+
+                $this->mailer->addAddress($minister['email']);
+                $this->mailer->Subject = $subject;
+                $this->mailer->Body = $msg;
+                $this->mailer->send();
             } catch (Exception $e) {
-                throw new \Exception($this->mail->ErrorInfo);
+                throw new \Exception($this->mailer->ErrorInfo);
+            }
+        }
+    }
+
+    public function sendErrorMsg($msg) {
+        $this->initialize();
+
+        $conf = Config::$mail['error'];
+        foreach ($conf['admin'] as $addr) {
+            try {
+                // Former settings should be cleared inside a loop
+                $this->mailer->clearAddresses();
+
+                $this->mailer->addAddress($addr);
+                $this->mailer->Subject = $conf['subject'];
+                $this->mailer->Body = $msg;
+                $this->mailer->send();
+            } catch (Exception $e) {
+                // Just do nothing if even the error message can't be sent = =
             }
         }
     }
